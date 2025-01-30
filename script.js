1169269c527440b8b3739018d82509da4c582631
manuelpezzi <pezzimanuel@gmail.com>
2025-01-30 12:41:41 +0100

    modale inserito html+js fine

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const gallery = document.getElementById('gallery')
+const modal = document.getElementById('modaleCustom')
+const modalBtn = document.getElementById('cancelFetch')
+const modalContent = document.querySelector(".modalContent");
+
 fetch("https://lanciweb.github.io/demo/api/pictures/")
     .then(response => response.json())
     .then(data => {
@@ -7,16 +11,33 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
             const col = document.createElement('div')
             col.className = "col-md-4 col-sm-6 d-flex justify-content-cener";
 
-            const polaroid =
-                `<div class ="polaroid">
-                     <img class="pin" src="img/pin.svg" alt="pin">
-                     <img class ="img-fluid photo" src="${pic.url}" alt="${pic.tiltle}">
+            const polaroid = document.createElement("div");
+            polaroid.className = 'polaroid';
+            polaroid.innerHTML =
+                `    <img class="pin" src="img/pin.svg" alt="pin">
+                     <img class ="img-fluid photo" src="${pic.url}" alt="${pic.title}">
                      <p>${pic.title}</p>
                      <p class = "data">${pic.date}</p>
         
         </div>
         `;
-            col.innerHTML = polaroid;
+            polaroid.querySelector('.photo').addEventListener('click', () => {
+                modalContent.innerHTML = `
+                     <img id="imgMoldal" class="imgFluid" src="${pic.url}" alt="foto modale">
+                <button id="cancelFetch" class="btn btn-danger mt-3"> chiudi</button>
+            </div>
+                    `;
+                modal.style.display = 'flex';
+
+                document.getElementById('cancelFetch').addEventListener('click', () => {
+                    modal.style.display = 'none';
+                })
+
+            });
+
+
+
+            col.appendChild(polaroid);
             gallery.appendChild(col);
 
         });
